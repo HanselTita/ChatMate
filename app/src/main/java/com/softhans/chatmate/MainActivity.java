d7973907bb341792a15a900db2058ac898869dec
Hansel Tita <hanseltita@gmail.com>
2019-05-29 13:47:12 +0700

    Initial commit

---
+++ app/src/main/java/com/softhans/chatmate/MainActivity.java
@@ -0,0 +1,58 @@
+package com.softhans.chatmate;
+
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mtoolbar;
+    private ViewPager myViewPAger;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabAccessorAdapter;
+
+    private FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mtoolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mtoolbar);
+        getSupportActionBar().setTitle("ChatMate");
+
+
+        myViewPAger = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPAger.setAdapter(myTabAccessorAdapter);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPAger);
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        if(currentUser ==null)
+        {
+            sendUserToLoginActivity();
+
+
+
+        }
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent LoginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(LoginIntent);
+    }
+}
