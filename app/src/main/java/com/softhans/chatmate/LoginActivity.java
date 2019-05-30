f9be207f86300a94d8eff23f87a8bc203d1a5efa
Hansel Tita <hanseltita@gmail.com>
2019-05-30 10:01:07 +0700

    link LoginActivity to RegisterActivity

--- app/src/main/java/com/softhans/chatmate/LoginActivity.java
+++ app/src/main/java/com/softhans/chatmate/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.softhans.chatmate;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
@@ -10,10 +15,42 @@ public class LoginActivity extends AppCompatActivity
 {
     private FirebaseUser currentUser;
 
+    private Button LoginBtn, PhoneLoginBtn;
+
+    private EditText UserEmail, Userpwd;
+    private TextView NeednewAccountLink, ForgetPwdLink;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        InitializeFiends();
+
+        NeednewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+              sendUserToRegisterActivity();
+            }
+        });
+    }
+
+
+    private void InitializeFiends()
+    {
+
+        LoginBtn = (Button) findViewById(R.id.login_button);
+        PhoneLoginBtn = (Button) findViewById(R.id.phone_login_button);
+        UserEmail = (EditText)findViewById(R.id.login_email);
+        Userpwd = (EditText)findViewById(R.id.login_password);
+
+
+        NeednewAccountLink = (TextView) findViewById(R.id.create_an_account_link);
+        ForgetPwdLink = (TextView) findViewById(R.id.forget_password_link);
+
     }
 
     @Override
@@ -33,7 +70,14 @@ public class LoginActivity extends AppCompatActivity
     private void sendUserToMainActivity()
     {
 
-        Intent MainIntent = new Intent(LoginActivity.this,MainActivity.class);
-        startActivity(MainIntent);
+        Intent LoginIntent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(LoginIntent);
+    }
+
+    private void sendUserToRegisterActivity()
+    {
+
+        Intent RegisterIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(RegisterIntent);
     }
 }
--- app/src/main/java/com/softhans/chatmate/RegisterActivity.java
+++ app/src/main/java/com/softhans/chatmate/RegisterActivity.java
@@ -1,13 +1,52 @@
 package com.softhans.chatmate;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button CreateAccountBtn;
+
+    private EditText UserEmail, Userpwd;
+    private TextView AlreadyHaveAccountLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        InitializeFiends();
+
+        AlreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sendUserToLoginActivity();
+            }
+        });
+
     }
+
+    private void InitializeFiends()
+    {
+
+        CreateAccountBtn = (Button) findViewById(R.id.register_button);
+        UserEmail = (EditText)findViewById(R.id.register_email);
+        Userpwd = (EditText)findViewById(R.id.register_password);
+        AlreadyHaveAccountLink = (TextView) findViewById(R.id.already_have_an_account_link);
+
+    }
+
+    private void sendUserToLoginActivity()
+    {
+
+        Intent LoginIntent = new Intent(RegisterActivity.this,LoginActivity.class);
+        startActivity(LoginIntent);
+    }
+
 }
